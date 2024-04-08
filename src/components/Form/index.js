be3ee1211dd225d1f0bfb04c2a9841ef6d5baa75
Germeson Martins <codemartins@outlook.com>
2024-04-08 00:10:14 -0300

    Style

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, Button } from "react-native"
+import { View, Text, TextInput,TouchableOpacity, Button } from "react-native"
 import ResultImc from "./ResultImc/"
+import styles from "./style"
 
 export default function Form(){
 
@@ -30,26 +31,32 @@ export default function Form(){
     }
 
     return(
-        <View>
-            <View>
-                <Text>Altura</Text>
-                <TextInput
+        <View style={styles.FormContext} >
+            <View style={styles.form} >
+                <Text style={styles.formLabel}>Altura</Text>
+                <TextInput style={styles.input}
                 onChangeText={setHeight}
                 value={height}
                 placeholder="Ex. 1.75"
                 keyboardType="numeric"/>
 
-                <Text>Peso</Text>
-                <TextInput
+                <Text style={styles.formLabel}>Peso</Text>
+                <TextInput style={styles.input}
                 onChangeText={setWeight}
                 value={weight}
                 placeholder="Ex. 75.36"
                 keyboardType="numeric"
                 />
                 
-                <Button
+                <TouchableOpacity
+                    style={styles.buttonCalculator}
+                    onPress={() => validationImc()}
+                    >
+                    <Text style={styles.textButtonCalculator } >Calcular</Text>
+                </TouchableOpacity>
+                {/* <Button
                 onPress={() => validationImc()}
-                title={textButton} />
+                title={textButton} /> */}
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc} />
         </View>
--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { View, Text } from "react-native"
+import Styles from "./style"
 
 export default function Title(){
     return(
-        <View>
-            <Text>ONEBITHEALTH!</Text>
+        <View style={Styles.boxTitle} >
+            <Text style={Styles.textTitle} >ONEBITHEALTH!</Text>
         </View>
     );
 }
